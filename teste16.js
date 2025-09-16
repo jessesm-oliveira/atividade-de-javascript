1a21da7070d2afbf4d6a42fa60bbeb92fa8f6960
jessesm-oliveira <jessemarques.oliveira91@gmail.com>
2025-09-15 22:05:10 -0300

    Delete teste16.js

--- teste16.js
+++
@@ -1,7 +0,0 @@
-// verifica se uma palavras é um palíndromo
-function verificarPalindromo(palavra) {
-    const palavraInvertida = palavra.split('').reverse().join('');
-    return palavra === palavraInvertida;
-}
-verificarPalindromo('arara'); // true
-verificarPalindromo('desenvolvimento'); // false    
